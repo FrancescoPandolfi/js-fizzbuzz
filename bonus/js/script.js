8f02da4f8ecc23eddad6e5ccd64b73e517306387
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-09 16:26:07 +0100

    Semplificazione

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,23 +7,20 @@ for (var i = 1; i < 101; i++) {
   // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
   if (i % 3 == 0 && i % 5 == 0) {
     document.getElementById('lista_numeri').innerHTML += "<li>" + "FizzBuzz" + "</li>";
-    i++;
   }
 
   // ma per i multipli di 3 stampi “Fizz” al posto del numero
-  if (i % 3 == 0) {
+  else if (i % 3 == 0) {
     console.log("Fizz");
     document.getElementById('lista_numeri').innerHTML += "<li>" + "Fizz" + "</li>";
-    i++;
   }
 
   // e per i multipli di 5 stampi Buzz.
-  if (i % 5 == 0) {
+  else if (i % 5 == 0) {
     document.getElementById('lista_numeri').innerHTML += "<li>" + "Buzz" + "</li>";
-    i++;
   }
 
-  if (i < 100) {
+  else {
     document.getElementById('lista_numeri').innerHTML += "<li>" + i + "</li>";
   }
 
--- js/script.js
+++ js/script.js
@@ -2,29 +2,26 @@
 
 var i = 1;
 
-while (i < 100) {
+while (i < 101) {
   // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
   if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
     document.getElementById('lista_numeri').innerHTML += "<li>" + "FizzBuzz" + "</li>";
-    i++;
   }
   // ma per i multipli di 3 stampi “Fizz” al posto del numero
-  if (i % 3 == 0) {
+  else if (i % 3 == 0) {
     console.log("Fizz");
     document.getElementById('lista_numeri').innerHTML += "<li>" + "Fizz" + "</li>";
-    i++;
   }
   // e per i multipli di 5 stampi Buzz.
-  if (i % 5 == 0) {
+  else if (i % 5 == 0) {
     console.log("Buzz");
     document.getElementById('lista_numeri').innerHTML += "<li>" + "Buzz" + "</li>";
-    i++;
   }
-  if (i < 100) {
+  else {
     document.getElementById('lista_numeri').innerHTML += "<li>" + i + "</li>";
     console.log(i);
   }
-  
+
   i++;
 }
